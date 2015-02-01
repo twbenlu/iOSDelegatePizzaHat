8ec0dc21ed35227d0f4feeb400b78b38a567d04f
benlu <benlu@benlutekiMacBook-Pro.local>
2015-02-01 15:14:10 +0800

    Initial Commit

---
+++ PizzaHat/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PizzaHat
+//
+//  Created by benlu on 2/1/15.
+//  Copyright (c) 2015 benlu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
